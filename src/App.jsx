ed3ed4c3629bc64be592130bdb92f8a491f11d9c
marco torremacco <mtorremacco@gmail.comm>
2025-02-05 15:55:41 +0100

    feat add delete btn and modify css

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,12 @@ function App() {
   }
   useEffect(fetchData, []);
 
+  const handleDelete = (id) => {
+    axios.delete(`http://localhost:3000/posts/${id}`).then(() => {
+      setFormData((current) => current.filter((item) => item.id !== id));
+    });
+  };
+
   return (
     <div className="container">
       <h1>Lista Spesa</h1>
@@ -20,7 +26,12 @@ function App() {
             <li>
               <strong>{item.titolo}</strong>
               <p>{item.immagine}</p> {item.contenuto}
-              <p>{item.tags}</p>
+              <p>
+                - {Array.isArray(item.tags) ? item.tags.join(" - ") : item.tags}
+              </p>
+              <div className="btn-container">
+                <button onClick={() => handleDelete(item.id)}>🗑 Elimina</button>
+              </div>
             </li>
           </div>
         ))}
